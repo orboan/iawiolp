40993408f48995be84743d59ec87a6d5b38e39ca
Oriol Boix Anfosso <dev@orboan.com>
2017-01-01 09:54:34 -0500

    First commit

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if(isset($_POST['email'])){
 	Big Picture by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	Modified and adapted by orboan.com
 -->
 <html>
 	<head>
